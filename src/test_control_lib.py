0e60d7a8ecf9ae6b2a8bd87213eab91292dc3ac7
Phitchawat Lukkanathiti <me@shayennn.com>
2019-06-15 06:26:09 +0700

    Test robot in realworld

--- src/test_control_lib.py
+++ src/test_control_lib.py
@@ -2,10 +2,12 @@
 
 from control_lib import Control
 import math
+import rospy
 
 
 def main():
-    control = Control('Test', True)
+    rospy.init_node('Test_Control_lib')
+    control = Control('Test')
     print(control.calcNewPosition(
         [0, 0, 0, 0, 0, math.pi/4], [1, -1, 0, 0, 0, 0]))
     print(control.calcNewPosition(
@@ -16,6 +18,27 @@ def main():
         [0, 0, 0, 0, 0, -math.pi/4], [1, -1, 0, 0, 0, 0]))
     print(control.calcNewPosition(
         [0, 0, 0, 0, 0, math.pi/6], [3, 2, 0, 0, 0, 0]))
+    rospy.loginfo("Robot will forward 2 meters.")
+    control.moveDist([2, 0, 0, 0, 0, 0])
+    control_wait()
+    rospy.loginfo("Robot will go deep 2 meters.")
+    control.moveDist([0, 0, -2, 0, 0, 0])
+    control_wait()
+    rospy.loginfo("Robot will go left 2 meters.")
+    control.moveDist([0, 2, 0, 0, 0, 0])
+    control_wait()
+    rospy.loginfo("Robot will turn 90 degree cww.")
+    control.moveDist([0, 0, 0, 0, 0, math.pi/2])
+    control_wait()
+
+
+def control_wait(control):
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        if control.isOkay():
+            break
+        r.sleep()
+    return True
 
 
 if __name__ == "__main__":
