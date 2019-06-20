a12cf95010431df451553fbdd02c42d8c971a70b
Phitchawat Lukkanathiti <me@shayennn.com>
2019-06-20 11:36:46 +0700

    Gate: Check for robot stability.

--- src/gate_lib.py
+++ src/gate_lib.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import rospy
 import time
+import math
 from zeabus.control.command_interfaces import CommandInterfaces
 
 
@@ -9,13 +10,15 @@ def mean(data):
     n = len(data)
     if n < 1:
         raise ValueError('mean requires at least one data point')
-    return sum(data)/n # in Python 2 use sum(data)/float(n)
+    return sum(data)/n  # in Python 2 use sum(data)/float(n)
+
 
 def _ss(data):
     """Return sum of square deviations of sequence data."""
     c = mean(data)
     ss = sum((x-c)**2 for x in data)
     return ss
+
 
 def stddev(data, ddof=0):
     """Calculates the population standard deviation
@@ -41,7 +44,8 @@ class Gate:
             'firstFinding': {
                 'threshold': 0.7,
                 'rotateAngle': 10*3.1416/180,
-                'maxAngle': 120*3.1416/180,  # Should be ~120 when switch is used.
+                # Should be ~120 when switch is used.
+                'maxAngle': 120*3.1416/180,
             },
             'forwardToGate': {
                 'cxThresold': 0.2,
@@ -50,7 +54,7 @@ class Gate:
                 'normalDist': 0.5,
                 'timeLimit': 30,
             },
-            'finalMoveDist': 2.0,
+            'finalMoveDist': 3.0,
             'endThreshold': 0.2,
             'visionStatusHistory': 20,
         }
@@ -96,6 +100,7 @@ class Gate:
                 self.setGateStatus(0)
             if self.getGateStatus() >= self.param['firstFinding']['threshold']:
                 self.control.reset_state()
+                self.gateYaw = self.control.current_pose[5]
                 rospy.loginfo('[LookToTheLeftLookToTheRight] Found gate.')
                 return True
             # Code when switch is available
@@ -111,8 +116,46 @@ class Gate:
                 rotate_count += self.param['firstFinding']['rotateAngle']
             r.sleep()
         self.control.reset_state()
-        self.control.absolute_yaw(self.control.current_pose[5])
         return True
+
+    def step01_5_lockYawToGate(self):
+        self.control.absolute_yaw(self.gateYaw)
+        yaws = [-100 for _ in range(40)]
+        r = rospy.Rate(10)
+        start = time.time()
+        rospy.loginfo('Waiting yaw to stable.')
+        while not rospy.is_shutdown():
+            yaws.append(self.control.current_pose[5])
+            del yaws[0]
+            if stddev(yaws) < 5*3.1416/180 and (max(yaws)-min(yaws) < 10):
+                break
+            if time.time()-start > 5:
+                break
+            r.sleep()
+        rospy.loginfo('Yaw is stable.')
+        widths = []
+        cxs = []
+        rospy.loginfo('Trying to locate the gate.')
+        while not rospy.is_shutdown():
+            vsResp = self.gate_proxy()
+            cxs.append(vsResp.cx1)
+            widths.append(abs(vsResp.x_left - vsResp.x_right))
+            if len(cxs) > 50:
+                break
+            r.sleep()
+        rospy.loginfo('Gate is located.')
+        rospy.loginfo('Adjusting heading.')
+        self.gateDist = self.estimateDist(sum(widths)/len(widths))
+        gateAngle = self.estimateAngle(sum(cxs)/len(cxs), self.gateDist)
+        self.gateYaw += gateAngle
+        self.control.absolute_yaw(self.gateYaw)
+
+    def estimateDist(self, width):
+        realGateW = 3.048  # meters
+        return realGateW/width/2
+
+    def estimateAngle(self, cx, dist):
+        return math.atan(cx/dist)
 
     def step02_forwardWithMoveLeftRight(self):
         """
@@ -122,7 +165,7 @@ class Gate:
                 move right until ... (might be 0.1)
             continue_forward_command
         """
-	yaws = [-100 for _ in range(40)]
+        yaws = [-100 for _ in range(40)]
         start = time.time()
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
@@ -132,18 +175,18 @@ class Gate:
             if stddev(yaws) > 5*3.1416/180:
                 r.sleep()
                 continue
-            if vsResp.found == 1:
-                self.setGateStatus(1)
-            else:
-                self.setGateStatus(0)
-            endCond = (self.isEnd() and
-                       (time.time()-start >
-                        self.param['forwardToGate']['timeLimit']))
-            if endCond:
-                self.control.reset_state()
-                rospy.loginfo('[GoToGate] Passed gate.')
-                return True
-            if self.control.check_xy(0.15, 0.15):
+            if self.control.check_xy(0.15, 0.15) and self.control.check_yaw(0.15):
+                if vsResp.found == 1:
+                    self.setGateStatus(1)
+                else:
+                    self.setGateStatus(0)
+                endCond = (self.isEnd() and
+                        (time.time()-start >
+                            self.param['forwardToGate']['timeLimit']))
+                if endCond:
+                    self.control.reset_state()
+                    rospy.loginfo('[GoToGate] Passed gate.')
+                    return True
                 if vsResp.cx1 < -self.param['forwardToGate']['cxThresold']:
                     rospy.loginfo("[GoToGate] Too left")
                     self.control.relative_xy(
