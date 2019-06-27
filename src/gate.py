795f2ecf6cc50da7c460d209fc263c9ca70a1de8
Supasan Komonlit <supasan_224@hotmail.com>
2019-06-28 00:26:50 +0700

    Prepare new file

---
+++ src/gate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# FILE			: gate.py
+# AUTHOR		: K.Supasan
+# CREATE ON		: 2019, June 27 (UTC+0)
+# MAINTAINER	: K.Supasan
+
+# README
+
+# REFERENCE
+
+from __future__ import print_function
+import rospy
+from zeabus.control.command_interfaces import CommandInterfaces
+from zeabus.vision.analysis_gate import AnalysisGate
