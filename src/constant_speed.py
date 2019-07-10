22008427f2803f5caea03c3452f027c81f237660
Supasan Komonlit <supasan_224@hotmail.com>
2019-07-11 02:26:07 +0700

    Finish renovate code

---
+++ src/constant_speed.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# FILE			: constant_speed.py
+# AUTHOR		: K.Supasan
+# CREATE ON		: 2019, July 10 (UTC+0)
+# MAINTAINER	: K.Supasan
+
+# README
+
+# REFERENCE
+
+# Constant for first mission
+_GATE_START_FORWARD_TIME_ = 6 
+_GATE_START_FORWARD_DISTANCE_ = 3
+_GATE_START_SURVEY_TIME_ = 6 
+_GATE_START_SURVEY_DISTANCE_ = 3
+_GATE_START_SURVEY_DIRECTION_ = 1.0 # positive is left and negative is right
+_GATE_FORWARD_ONLY_TIME_ = 15
+_GATE_FORWARD_ONLY_DISTANCE_ = 8
+_GATE_START_DEPTH_ = -0.75 
--- src/gate_speed.py
+++ src/gate_speed.py
@@ -11,7 +11,7 @@
 from __future__ import print_function
 from zeabus.control.command_interfaces import CommandInterfaces
 from zeabus.vision.analysis_gate import AnalysisGate
-from mission_constant import *
+from contant_speed import *
 
 import rospy
 
@@ -28,112 +28,125 @@ class Gate:
 
         self.save_point_x = 0
         self.save_point_y = 0
-        
 
     def start_mission( self ): # state mission is 0
 
-        self.control.publish_data( "START_MISSION doing mission gate")
+        self.control.publish_data( "START doing mission gate")
 
         self.control.reset_state( 0 , 0 )
 
-        self.control.absolute_z( -0.8 )
+        self.control.absolute_z( _GATE_START_DEPTH_ )
 
-        self.control.publish_data( "START_MISSION waiting ok depth")
-        while( not self.control.check_z( 0.15 ) ):
-            self.rate.sleep()
+        self.control.publish_data( "START survey to find mission by survey" )
 
-        self.start_constant()
+        self.control.publish_data( "START Waiting yaw are ok")
 
-    def start_constant( self ):
-        self.control.publish_data( "START_CONSTANT survey to find mission by survey" )
-
-        self.control.publish_data( "START_CONSTANT Waiting yaw are ok")
         while( not self.control.check_yaw( 0.15 ) ):
             self.rate.sleep()
 
-        self.control.publish_data( "START_CONSTANT Waiting xy are ok")
-        while( not self.control.check_xy( 0.15 , 0.15 ) ):
-            self.rate.sleep()
+        self.control.publish_data( "START DEACTIVATE XY and forward")
+        self.control.deactivate( ['x' , 'y'] )
 
-        self.control.publish_data( "START_CONSTANT start forward " + str( GATE_START_FORWARD ) )
-        self.control.relative_xy( GATE_START_FORWARD , 0 )
-        self.rate.sleep()
-        count = 0 
-        while( ( not self.control.check_xy( 0.15 , 0.15 ) ) and count < 3):
+        # Start to forward by parameter and fix to move forward only
+        self.control.force_xy( 1.2 , 0 , True )
+        count = 0
+        start_time = rospy.get_rostime()
+        diff_time = ( rospy.get_rostime() - start_time ).to_sec()  
+        while( diff_time < _GATE_START_FORWARD_TIME_ and count < 3):
             self.rate.sleep()
 
             self.vision.call_data()
             self.vision.echo_data()
             if( self.vision.result['found'] ):
-                self.control.publish_data( "START_CONSTANT Found picture")
                 count += 1
+                self.control.publish_data( "START Found picture count is " + str( count ) )
             else:
                 count = 0
 
+            if( self.control.force_xy( 1.2 , 0 ) > _GATE_START_FORWARD_DISTANCE_ ):
+                self.control.publish_data( "START Abort forward by distance")
+                break
+
+            diff_time = ( rospy.get_rostime() - start_time ).to_sec()  
+
         if( count == 3 ):
-            self.control.relative_xy( 0 , 0 )
-            self.control.publish_data( "START_CONSTANT Don't move I find target")
+            self.control.publish_data( "START Move to target picture")
+            while( not rospy.is_shutdown() ):
+                self.rate.sleep()
+                self.vision.call_data()
+                self.vision.echo_data()
+                if( self.vision.result['center_x'] < -50 ):
+                    self.control.force_xy( 0.3 , 1.5 )
+                elif( self.vision.result[ 'center_x' ] > 50 ):
+                    self.control.force_xy( 0.3 , -1.5 )
+                else:
+                    self.control.publish_data( "START Now center change to lock target")
         else:
-            self.control.publish_data( "START_CONSTANT start survey " + str( GATE_START_SURVEY) )
-            self.control.relative_xy( 0 , GATE_START_SURVEY )
+            self.control.publish_data( "START survey to gate mission" )
+            self.control.force_xy( 0 , 1.5 * _GATE_START_SURVEY_DIRECTION_ , True )
             count = 0 
 
-        while( ( not self.control.check_xy( 0.15 , 0.15 ) ) and count < 3 ):
+        # Next If you don't found picture and count == 3 I will survey
+        start_time = rospy.get_rostime()
+        diff_time = ( rospy.get_rostime() - start_time ).to_sec()  
+        while( diff_time < _GATE_START_SURVEY_TIME_ and count < 3 ):
             self.rate.sleep()
     
             self.vision.call_data()
             self.vision.echo_data()
         
             if( self.vision.result['found'] ):
-                self.control.publish_data( "START_CONSTANT Found picture")
+                self.control.publish_data( "START Found picture")
                 count += 1
             else:
                 count = 0
 
+            if( self.control.force_xy( 0 , 1.5 * _GATE_START_SURVEY_DIRECTION_ ) 
+                    > _GATE_START_SURVEY_DISTANCE_ ):
+                self.control.publish_data( "START Abort forward by distance")
+                break
+
+            diff_time = ( rospy.get_rostime() - start_time ).to_sec()  
+
         if( count == 3 ):
-            temp_relative_x = 0
-            temp_relative_y = ( self.vision.result[ 'center_x' ] * 2.0 )/ -100
-            if( self.vision.result[ 'distance' ] > 4 ):
-                temp_relative_x = 1
-            else:
-                temp_relative_x = self.vision.result['distance'] / 2
-            
-            self.control.relative_xy( temp_relative_x , temp_relative_y )
-            self.control.publish_data( "START_CONSTANT I will move " + str( temp_relative_x ) 
-                + " and move y  " + str( temp_relative_y ) )
+            self.control.publish_data( "START next function is lock_target" )
             self.lock_target()
         else:
+            self.control.publish_data( "START next function is direct_search" )
             self.direct_search()
 
         
     def direct_search( self ):
         self.control.publish_data( "DIRECT_SEARCH start to search" )
 
+        self.control.force_false()
+
         self.control.publish_data( "DIRECT_SEARCH waiting yaw" )
         while( not self.control.check_yaw( 0.15 ) ):
             self.rate.sleep()
 
-        self.control.publish_data( "DIRECT_SEARCH waitign xy" )
-        while( not self.control.check_xy( 0.15 , 0.15 ) ):     
-            self.rate.sleep()
-
-        self.control.publish_data( "COMMAND TO MOVE DIRECT TO GATE " 
-            + str( GATE_LIMIT_DISTANCE ) + " meters" )
-
         count = 0
-        while( ( not self.control.check_xy( 0.15 , 0.15 ) ) and ( count < 3 ) ):
+        start_time = rospy.get_rostime()
+        diff_time = ( rospy.get_rostime() - start_time ).to_sec() 
+        self.control.publish_data( "DIRECT_SEARCH start to forward")
+        self.control.force_xy( 1.2 , 0 , True )
+        while( ( diff_time < _GATE_FORWARD_ONLY_TIME_ ) and ( count < 3 ) ):
             self.rate.sleep()
             self.vision.call_data()
             self.vision.echo_data()
 
             if( self.vision.result[ 'found' ] ):
                 count += 1
+                continue
             else:
                 count = 0
 
+            if( self.control.force_xy( 1.2 , 0 ) < _GATE_FORWARD_ONLY_DISTANCE_ ):
+                self.control.publish_data( "DIRECT_SEARCH abort by distance")
+                break
+
         if( count == 3 ):
-            self.control.publish_data( "We found the picture so I will reset state")
-            self.control.reset_state( 0 , 0)
+            self.control.force_false()
             self.lock_target()
         else:
             self.control.publish_data( "We don't found picture abort that" )
@@ -141,16 +154,6 @@ class Gate:
 
     def lock_target( self ):
         self.control.publish_data( "LOCK_TARGET start mission")
-
-        self.control.publish_data( "LOCK_TARGET waiting xy are ok")
-        while( not self.control.check_xy( 0.15  , 0.15 ) ):
-            self.rate.sleep()
-    
-        self.control.publish_data( "LOCK_TARGET waiting yaw are ok")
-        while( not self.control.check_yaw( 0.15 ) ):
-            self.rate.sleep()
-    
-        self.control.deactivate( [ 'x' , 'y' ] )
 
         count_unfound = 0
         self.last_distance_to_found = 0
@@ -176,6 +179,18 @@ class Gate:
                     self.control.publish_data( "LOCK_TARGET near target I break now" )
                     self.rate.sleep()
                     break
+
+                relative_z = 0
+                if( self.vision.result['center_y'] < -20 ):
+                    relative_z = -0.15
+                elif( self.vision.result['center_y'] > 20 ):
+                    relative_z = 0.15
+                else:
+                    relative_z = 0
+
+                if( ( relative_z != 0 ) and self.control.check_z( 0.15 ) ):
+                    self.control.publish_data( "LOCK_TARGET relative z : " + str( relative_z ) )
+
             else:
                 count_unfound += 1
                 self.control.publish_data( "LOCK_TARGET count_unfound " + str( count_unfound ) )
@@ -186,15 +201,10 @@ class Gate:
                 self.control.publish_data( "LOCK_TARGET breaking down don't found gate" )
                 break
 
+        self.control.force_false()
         self.control.activate( ['x' , 'y'])
         self.control.publish_data( "LOCK_TARGET to front gate {:4.2f}".format( 
             self.last_distance_to_found ) )
-        if( self.last_distance_to_found < 2.2 ):
-            self.control.publish_data( "LOCK_TARGET Don't move last because it near")
-        else:
-            self.control.publish_data( "LOCK_TARGET Move " 
-                + str( self.last_distance_to_found - 1 ) )
-            self.control.relative_xy( self.last_distance_to_found - 1.5 , 0 )
 
     def force_interfaces( self , x , y ): # Use for print abotu when force have change 
         if( ( self.save_point_x != 0 or self.save_point_y != y ) ):
@@ -204,6 +214,8 @@ class Gate:
             self.save_point_y = y
         self.control.force_xy( 1.0*x/100 , 1.0*y/100 )
 
+
+    # Below function is manual call
     def pass_target( self ):
         self.control.publish_data( "PASS_TARGET now is last step" )
 
