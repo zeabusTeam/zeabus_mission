f22c770f85b1c7056c5a8eb30df4a02c0eb76727
Supasan Komonlit <supasan_224@hotmail.com>
2019-07-26 20:52:00 +0700

    Updated parameter

--- src/constant_speed.py
+++ src/constant_speed.py
@@ -28,7 +28,7 @@ SUPER_LEFT = 2.3
 SUPER_RIGHT = -2.3
 
 # Constant for first mission is mission gate
-GATE_START_DEPTH = -0.5            # This use to target depth of this mission
+GATE_START_DEPTH = -0.75            # This use to target depth of this mission
 GATE_START_FORWARD_TIME = 9         # This is limit of time to go forward
 GATE_START_FORWARD_DISTANCE = 3     # This is limit of distance to go forward
 GATE_START_SURVEY_TIME = 20         # This is limit time for go servey 
@@ -47,9 +47,10 @@ PATH_START_DEPTH = -1               # This will tell desire depth when want to s
 PATH_TARGET_DEPTH = -2.4            # This target of path will want to go before do mission
 PATH_FIND_TIME = 5                  # Left is constant but right + 2 second
 PATH_FORCE_YAW = 0.2                # This variable is force to use rotation
-PATH_OK_DIFF_YAW = 0.2             # This use ok yaw to don't rotation again 
+PATH_OK_DIFF_YAW = 0.21             # This use ok yaw to don't rotation again 
 PATH_LAST_TIME = 5
 PATH_END_DEPTH = -1.5               # This is absolute depth after do task path finish
+PATH_MODE = True                    # If true mean you don't use mode tracking to each point
 
 # Constant for mission buoy
 BUOY_START_DEPTH = -1.8             # This will tell desire depth when want to start this mission
@@ -84,8 +85,8 @@ DROP_TIME_OPEN = 4                  # This is time to command same force
 # ----> Drop force yaw
 DROP_FORCE_YAW = 0.2         
 # ----> Below 3 constant is use about estimate center to do mission
-DROP_CENTER_X_DROP = -40
-DROP_CENTER_X_OPEN = 20
+DROP_CENTER_X_DROP = -20
+DROP_CENTER_X_OPEN = 40
 DROP_CENTER_Y = 55 # target of center y when you want to drop and can use for estimate open
 
 # Constant for operator exposed
@@ -124,27 +125,29 @@ STAKE_BACKWARD_TIME = 4
 STAKE_HEART_CENTER_Y = -10
 
 # Constant for connect mission
+# ====> Mission Path 
+STRATEGY_NO_PATH = True
 # ====> Mission Gate
 STRATEGY_TIME_SURVEY_PATH = 2               # Time to survey before pass gate
 STRATEGY_FORCE_SURVEY_PATH = SUPER_RIGHT    # Force to survey before pass gate
 STRATEGY_TIME_GATE_PATH = 30                # Time to forward and doing pass gate
 STRATEGY_FORCE_GATE_PATH = SUPER_FORWARD    # Force to forward and doing pass gate
-STRATEGY_ROTATION_GATE_BUOY = math.pi / 2   # Use rotation when don't found path
+STRATEGY_ROTATION_GATE_BUOY = -math.pi / 4   # Use rotation when don't found path
 # ====> Mission Buoy
 STRATEGY_DEPTH_BOUY = BUOY_START_DEPTH      # This depth will use in buoy mission
-STRATEGY_TIME_BUOY = 2                      # This is time to forward search direct after path
+STRATEGY_TIME_BUOY = 1                      # This is time to forward search direct after path
 STRATEGY_FORCE_BUOY = SURVEY_FORWARD        # This is force forward search direct after path
-STRATEGY_TIME_BUOY_SURVEY = 7               # This is time to survey after path to buoy
+STRATEGY_TIME_BUOY_SURVEY = 6               # This is time to survey after path to buoy
 STRATEGY_FORCE_BUOY_SURVEY = SURVEY_LEFT    # This is force survey after path to buoy
-STRATEGY_TIME_BUOY_PATH = 10                # This is time to use forward find buoy only direct 
+STRATEGY_TIME_BUOY_PATH = 2                # This is time to use forward find buoy only direct 
 STRATEGY_FORCE_BUOY_PATH = SURVEY_FORWARD   # This is force use to direct after buoy to search path
 # ====> Mission DROP
-STRATEGY_ROTATION_BUOY_DROP = math.pi/-4.0  # This use rotation buoy to drop in case don't find path
+STRATEGY_ROTATION_BUOY_DROP = math.pi/2.0  # This use rotation buoy to drop in case don't find path
 STRATEGY_DEPTH_FIND_DROP = DROP_FIND_DEPTH  # This use to depth for find depth
-STRATEGY_FREE_TIME_DROP = 6
+STRATEGY_FREE_TIME_DROP = 12
 STRATEGY_FREE_FORCE_DROP = SUPER_FORWARD
 STRATEGY_FORCE_DROP = SURVEY_FORWARD
-STRATEGY_TIME_DROP = 20
+STRATEGY_TIME_DROP = 30
 STRATEGY_DISTANCE_DROP = 6
 # ====> Mission Exposed
 STRATEGY_EXPOSED_FIND = EXPOSED_START_DEPTH
