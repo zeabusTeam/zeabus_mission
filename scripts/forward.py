b8fe6fd81dea58d838a6bd55a1d58bc4930d4947
Robot <zeabus.auv@gmail.com>
2019-12-31 04:18:30 +0700

    Finish forward for video

--- scripts/forward.py
+++ scripts/forward.py
@@ -28,24 +28,29 @@ class Forward:
 
         self.publish_state = rospy.Publisher( "/activate" , Bool , queue_size = 1 )
 
+
     def activate( self ):
 
-        print( "Start count time before start 30 second" )
+        rate = rospy.Rate( 15 )
 
-        self.ch.reset_all( True )
+        print( "Start count time before start 30 second" )
 
         self.ch.activate( False )
         self.ch.pub( "Client close control" )
         count = 0
-        while count < START_TIME :
+        while count < START_TIME and not rospy.is_shutdown() :
             rospy.sleep( 1.0 )
             count += 1
             print( "Count is " + str( count ) + " seconds" )
         self.ch.activate( True )
         self.ch.pub( "Client open control" )
 
+        self.ch.reset_all( True )
+
+        rate.sleep()
         print( "set roll to 0" )
         self.ch.absolute_roll( 0 )
+        rate.sleep()
         print( "set pitch to 0" )
         self.ch.absolute_pitch( 0 )
     
@@ -55,28 +60,32 @@ class Forward:
         self.ch.pub( "Client command relative depth is -1.0 sleep for 2 second" )
         rospy.sleep( 2.0 )
 
-        self.ch.pub( "Client command forward by force 4")
-        self.ch.plane_xy( 4.0 )
+        self.ch.pub( "Client command forward by force 5")
+        self.ch.plane_xy( 7.0 )
         print( "Sleep for 11 second")
         count = 0
-        while count < 10:
+        while count < 15:
             rospy.sleep( 1.0 )
             count += 1
             print( "Count is " + str( count ) + " seconds" )
         self.ch.plane_xy( 0.0 )
         self.ch.pub( "Client command forward by 0" )
         
-        self.ch.absolute_pitch( -1.57 )
-        self.ch.pub( "Clinet command absolute pitch is -1.57" )
-        print( "Sleep for 2 second" )
-        count = 0
-        rospy.sleep( 2.0 )
+#        self.ch.absolute_pitch( -1.57 )
+#        self.ch.pub( "Clinet command absolute pitch is -1.57" )
+#        print( "Sleep for 2 second" )
+#        count = 0
+#        rospy.sleep( 2.0 )
+
+        self.ch.pub( "Client shutdown force" )
+        self.ch.plane_xy( 0 )
+        rospy.sleep( 5 )
  
         self.ch.pub( "Client command forward by force -4")
-        self.ch.plane_xy( -4.0 )
+        self.ch.plane_xy( -6.0 )
         print( "Sleep for 11 second")
         count = 0
-        while count < 10:
+        while count < 50:
             rospy.sleep( 1.0 )
             count += 1
             print( "Count is " + str( count ) + " seconds" )
